9e67ae552a9d422fea493e8a09e3502f2f85241a
francisco.mendonca <francisco.mendonca@dataprev.gov.br>
2025-11-13 17:55:01 -0300

    add interfaces initial project

---
+++ 16-interfaces/exercicio/src/model/services/ContractService.java
@@ -0,0 +1,12 @@
+package model.services;
+
+import model.entities.Contract;
+
+public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+    private Contract contract;
+
+    public void processContract(Contract contract, Integer months) {
+        contract.
+    }
+}
